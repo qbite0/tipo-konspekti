d4e85227d2b2bd2a30444e34afa3285d1e4f1f43
Max Quiet <qbite0@gmail.com>
2022-09-26 10:59:00 +0700

    da

--- system-programming/lecture3/works/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ system-programming/lecture3/works/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,62 +1,36 @@
-﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <process.h>
 
+const int n = 0;
+const int P = 0;
+const int l = 0;
+
+const unsigned int x = 3;
+const unsigned int y = 3;
+
+int a[x][y];
+int b[x][y];
+int c[x][y];
+
 int main() {
-	const int n = 0;
-	int a[10][10];
-	int b[10][10];
-	int ary c = 10;
-
-	for (int i = 0; i < n - 1; i++){
-		// параллельное вычисление строк
-		for (int j = 0; i < n - 1; i++) {
-			c[i,j] = 0.0;
-			for (int k = 0; k < n - 1; k++)
-				c[i,j] = c[i,j] + a[i,k] * b[k,j];
-		}
-	}
-
-	for (i = 0 to n - 1, j = 0 to n - 1)
-	{
-		// все  строки и
-		с[i,j] = 0.О; // все  столбцы
-
-		for[k = 0  to n - 1] {
-			c[i,j] = c[i,j] + a[i,k] * b[k,j];
-		}
-	}
-
-	for[i = 0 to n - 1]{
+	for (int i = 0; i < n - 1; i++) {
 		// строки параллельно, затем со
-		for[j = 0 to n - 1] {
+		for (int j = 0; j < n - 1; j++) {
 			// столбцы параллельно
-			c[i,j] = 0.0;
-			for[k = 0  to n - 1]
-				c[i,j] = c[i,j] + a[i,k] * b[k,j];
+			c[i][j] = 0;
+			for(int k = 0; k < n - 1; k++)
+				c[i][j] = c[i][j] + a[i][k] * b[k][j];
 		}
 	}
-
-	process row[i = 0 to n - 1]{
-		//строки параллельно
-		for[j = 0 to n - 1] {
-			c[i,j] = 0.0;
-			for[k = 0  to n - 1]
-				c[i,j] = c[i,j] + a[i,k] * b[k,j];
-		}
-	}
-
-	process worker[w = 1 to P]{ //#полосы параллельно
+	for(int w = 1; w < P; w++) { //#полосы параллельно
 		int first = (w - 1) * n / P; // первая строка полосы
 		int last = first + n / P - 1; // последняя строка полосы
 
-		for [i = first to last] {
-			for (j = 0 to n - l) {
-				c[i, j] = 0.0;
-				for[k = 0 to n - 1]
-					c[i, j) = c[i, j) + a[i, k] * b[k, j);
+		for (int i = first; i < last; i++) {
+			for (int j = 0; j < n - l; j++) {
+				c[i][j] = 0;
+				for (int k = 0; k < n - 1; k++)
+					c[i][j] = c[i][j] + a[i][k] * b[k][j];
 			}
 		}
 	}
---
+++ Новая папка (2)/lecture1.md
@@ -0,0 +1,4 @@
+mysql 8.0
+Android Studio 7
+Visual studio 2019
+visio 2016
